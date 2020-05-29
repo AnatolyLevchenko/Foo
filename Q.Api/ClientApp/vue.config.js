fe28d459c6d40a0d41b3874285066b9905fce26f
AnatolyLevchenko <kypiwindy@xaker.ru>
2020-05-29 09:56:21 +0300

    hot reload

--- Q.Api/ClientApp/vue.config.js
+++ Q.Api/ClientApp/vue.config.js
@@ -1,5 +1,17 @@
 ﻿module.exports = {
     outputDir: "../wwwroot/dist",
-    publicPath: "../dist/",
-    filenameHashing: false
+    publicPath: "/dist/",
+    filenameHashing: false,
+    devServer: {
+        proxy: {
+            '/': {
+                target: 'http://localhost:51062',
+              //  ws: true,
+                changeOrigin: true
+            },
+           
+
+        },
+        open: true,
+    }
 }
